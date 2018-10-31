800e0718a290ba1bf5f3b13dcc76a4d86c3d9776
Chevion Lu <chevionlu@gmail.com>
2018-10-31 18:30:55 +0800

    Google analytics

--- src/popup.js
+++ src/popup.js
@@ -10,20 +10,12 @@ new Vue({
 /**
  * Google 数据分析
  */
-(function() {
-  setTimeout(() => {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://www.googletagmanager.com/gtag/js?id=UA-128079841-1';
-    
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-    ga.onload = function() {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-128079841-1');
-    }
-  }, 600);
-})();
+setTimeout(() => {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-128079841-1', 'auto')
+  ga('set', 'checkProtocolTask', null)
+  ga('send', 'pageview')
+}, 600);
