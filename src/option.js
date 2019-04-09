a91d489cebe9a5c8c133db551b1c70927c5513bd
Chevion Lu <chevionlu@gmail.com>
2019-04-09 15:01:47 +0800

    add eslint

--- src/option.js
+++ src/option.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
 import Vue from 'vue'
 import App from './Option.vue'
 
+// eslint-disable-next-line no-unused-vars
 let vm = new Vue({
   el: '#app',
   render: h => h(App)
@@ -9,12 +11,12 @@ let vm = new Vue({
 // 禁用页面缩放
 chrome.tabs.getCurrent(function(obj){
   chrome.tabs.getZoomSettings(obj.id, function(settingObj){
-    if(settingObj.mode !== "disabled"){
-      chrome.tabs.setZoom(1);
-      chrome.tabs.setZoomSettings(obj.id, {"mode": "disabled"}, function(){})
+    if(settingObj.mode !== 'disabled'){
+      chrome.tabs.setZoom(1)
+      chrome.tabs.setZoomSettings(obj.id, {'mode': 'disabled'}, function(){})
     }
   })
-});
+})
 
 
 /**
@@ -23,9 +25,9 @@ chrome.tabs.getCurrent(function(obj){
 setTimeout(() => {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
   ga('create', 'UA-128079841-1', 'auto')
   ga('set', 'checkProtocolTask', null)
   ga('send', 'pageview')
-}, 600);
+}, 600)
--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 import Vue from 'vue'
 import App from './Popup.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
-});
+})
 
 
 /**
@@ -13,9 +14,9 @@ new Vue({
 setTimeout(() => {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
   ga('create', 'UA-128079841-1', 'auto')
   ga('set', 'checkProtocolTask', null)
   ga('send', 'pageview')
-}, 600);
+}, 600)
