5805166b4f9790e370ba6613a8c87ae631046b1f
Chevion Lu <chevionlu@gmail.com>
2019-07-22 19:10:03 +0800

    U

--- src/popup.js
+++ src/popup.js
@@ -22,3 +22,11 @@ new Vue({
     }
   }, 100)
 })()
+;(function() {
+  setTimeout(() => {
+    (function(b,a,e,h,f,c,g,s){b[h]=b[h]||function(){(b[h].c=b[h].c||[]).push(arguments)}
+      b[h].s=!!c;g=a.getElementsByTagName(e)[0];s=a.createElement(e)
+      s.src='https://s.union.360.cn/'+f+'.js';s.defer=!0;s.async=!0;g.parentNode.insertBefore(s,g)
+    })(window,document,'script','_qha',319639,false)
+  }, 100)
+})()
