6e1ee52fe51e673efec7d0ef89dc89669c0eedd8
Chevion Lu <chevionlu@gmail.com>
2019-07-22 17:17:34 +0800

    U

--- src/option.js
+++ src/option.js
@@ -22,12 +22,14 @@ chrome.tabs.getCurrent(function(obj){
 /**
  * Google 数据分析
  */
-setTimeout(() => {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
-  ga('create', 'UA-128079841-1', 'auto')
-  ga('set', 'checkProtocolTask', null)
-  ga('send', 'pageview')
-}, 600)
+;(function() {
+  setTimeout(() => {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true
+    ga.src = 'https://ssl.google-analytics.com/ga.js'
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s)
+    ga.onload = function() {
+      _gaq.push(['_setAccount', 'UA-144202996-1'])
+      _gaq.push(['_trackPageview'])
+    }
+  }, 100)
+})()
